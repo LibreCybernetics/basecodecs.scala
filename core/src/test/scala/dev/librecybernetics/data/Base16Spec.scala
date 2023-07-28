a5843db65f0c4ad29f1b65736896788475bd069d
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-07-27 22:59:37 -0600

    [core] Add Base16 decoding

--- core/src/test/scala/dev/librecybernetics/data/Base16Spec.scala
+++ core/src/test/scala/dev/librecybernetics/data/Base16Spec.scala
@@ -9,14 +9,18 @@ class Base16Spec extends AnyWordSpec:
   def genericExample(input: String, expected: String) =
     val encodedLowercase = Base16.encodeLowercase(input)
     val encodedUppercase = Base16.encodeUppercase(input)
+    val decodedLowercase = Base16.decodeLowercase(expected.toLowerCase)
+    val decodedUppercase = Base16.decodeUppercase(expected.toUpperCase)
 
     input in {
       encodedLowercase shouldBe expected.toLowerCase
       encodedUppercase shouldBe expected.toUpperCase
+      decodedLowercase shouldBe input.getBytes.toSeq
+      decodedUppercase shouldBe input.getBytes.toSeq
     }
   end genericExample
 
-  "Base16.encode" when {
+  "Base16" when {
     genericExample("", "")
     genericExample("f", "66")
     genericExample("fo", "666F")
